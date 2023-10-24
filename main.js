eac1a4b4a088cba74613520a1854f4a1bf557215
NihadFatiyev <tu7NAAWDF@code.edu.az>
2023-10-24 13:25:00 +0400

    i

---
+++ main.js
@@ -0,0 +1,31 @@
+//1
+let id = document.getElementById("ferid");
+
+//2
+// function myfun() {
+//     alert('hello')
+// }
+
+//3
+// id.style.backgroundColor = 'red'
+
+// function myfun() {
+//     document.getElementById('ferid').style.backgroundColor = 'red'
+
+// }
+
+
+// 4
+
+// function myFunction() {
+//     var element = document.getElementById("ferid");
+//     element.classList.add("mystyle");
+// }
+
+// 5
+// function myFunction() {
+//     var element = document.getElementById("ferid");
+//     element.classList.toggle("mystyle");
+// }
+
+//6
